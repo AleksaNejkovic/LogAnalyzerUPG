65ce3e5dceedbaed36ba01e3bad1eefdaa86f9e2
rl190220d <rl190220d@student.etf.bg.ac.rs>
2023-11-07 04:58:50 +0100

    Add file path. Add log folder into console app

--- LogAnalyzerUPG/Program.cs
+++ LogAnalyzerUPG/Program.cs
@@ -1,14 +1,19 @@
 ﻿using LogAnalyzerLogic;
 using LogAnalyzerConstantsLibrary;
+using System.Resources;
+using System.Reflection;
 
 IFileUtil fileUtil = new FileUtil();
 IDisplayUtil displayUtil = new DisplayUtil();
 ILogParser logParser = new LogParser(fileUtil);
 
+string LOG_DIR_ex040730 = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName, "LogFile", "ex040730.log");
+string LOG_DIR_ex120326 = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName, "LogFile", "ex120326.log");
+
 Console.WriteLine("Parsed file name: ex040730.log");
-displayUtil.PrintParsingResult(logParser.ParseLogFile(LogAnalyzerConstants.LOG_DIR_ex040730));
+displayUtil.PrintParsingResult(logParser.ParseLogFile(LOG_DIR_ex040730));
 Console.WriteLine("Parsed file name: ex120326.log");
-displayUtil.PrintParsingResult(logParser.ParseLogFile(LogAnalyzerConstants.LOG_DIR_ex120326));
+displayUtil.PrintParsingResult(logParser.ParseLogFile(LOG_DIR_ex120326));
 
 
-Console.ReadLine(); //Avoid closing window
+Console.ReadLine();
